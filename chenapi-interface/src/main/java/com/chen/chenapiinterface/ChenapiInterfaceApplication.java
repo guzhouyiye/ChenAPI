d4536501b3c40dea7dd295a06bd0570ccf46c63e
guzhouyiye <1581241485@qq.com>
2024-11-04 08:59:17 +0800

    测试SDK

---
+++ chenapi-interface/src/main/java/com/chen/chenapiinterface/ChenapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.chen.chenapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChenapiInterfaceApplication.class, args);
+    }
+
+}
